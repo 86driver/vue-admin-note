18941a5d86c1d4ec38a02b5e38816d5fb62018b6
86driver <610410574@qq.com>
2019-03-07 22:34:37 +0800

    日常修改

--- vue-admin/build/webpack.prod.conf.js
+++ vue-admin/build/webpack.prod.conf.js
@@ -17,7 +17,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
-      extract: true,
+      extract: false, //修复图标不显示问题
       usePostCSS: true
     })
   },
